8c7a18c0614af8e4c3c0490fea4dc7f461d231c4
Kelly <kelly_liu@berkeley.edu>
2021-01-09 18:35:02 -0500

    homepage buttons

---
+++ my-extension/src/Components/Background.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import background from "../assets/images/Onboarding.png"
+
+import "../assets/css/background.css"
+
+const Background = () => {
+    return (
+    <>
+        <img src = {background} id="backgroundpic"/>
+    </>
+    );
+}
+
+
+export default Background;
